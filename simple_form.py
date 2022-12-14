73b0892605503cc57a0a4cd9ea78b35bb29a72f5
stumn <pukut0317@gmail.com>
2022-12-14 15:00:12 +0900

    足し算フォーム完成

--- simple_form.py
+++ simple_form.py
@@ -8,10 +8,11 @@ def index():
 
 @app.route('/procForm', methods=['POST'])
 def processPost():
-    msg = request.form['msg']
-    return render_template("result.html", massage = msg)
+    msg1 = request.form['msg1']
+    msg2 = request.form['msg2']
+    msg1 = int(msg1)
+    msg2 = int(msg2)
+    return render_template("result.html", message = msg1 + msg2)
 
 if __name__ == '__main__':
     app.run(host= "0.0.0.0", port=8000, debug = True)
-
-
